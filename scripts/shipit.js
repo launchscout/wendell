0df7f7bedebb967e4e821c954a46b0044a88dc2c
Tim Mecklem <timothy@mecklem.com>
2019-01-04 18:22:25 -0500

    Add shipit

---
+++ scripts/shipit.js
@@ -0,0 +1,39 @@
+// Description:
+//   Rodent Motivation
+//
+//   Set the environment variable HUBOT_SHIP_EXTRA_SQUIRRELS (to anything)
+//   for additional motivation
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   HUBOT_SHIP_EXTRA_SQUIRRELS
+//
+// Commands:
+//   ship it - Display a motivation squirrel
+//
+// Author:
+//   maddox
+
+const squirrels = [
+  'http://shipitsquirrel.github.io/images/ship%20it%20squirrel.png',
+  'http://28.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg',
+  'http://i.imgur.com/DPVM1.png',
+  'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
+  'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
+  'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+  'http://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg'
+]
+
+module.exports = function (robot) {
+  // Enable a looser regex if environment variable is set
+  let regex
+  if (process.env.HUBOT_SHIP_EXTRA_SQUIRRELS) {
+    regex = /\bship(ping|z|s|ped)?\s*it\b/i
+  } else {
+    regex = /\bship\s*it\b/i
+  }
+
+  return robot.hear(regex, msg => msg.send(msg.random(squirrels)))
+}
