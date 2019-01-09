6f515c6c04e4f35388deb3143dbf4fa70315c69a
Tim M <tim.mecklem+gitkraken@gmail.com>
2019-01-09 09:10:09 -0500

    Start The Office

---
+++ scripts/the-office.js
@@ -0,0 +1,19 @@
+// Description:
+//   The Office references
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   pizza by alfredo - it's a hot circle of garbage
+//
+// Author:
+//   Tim
+
+module.exports = robot =>
+  robot.hear(/\bpizza by alfredo\b/i, (msg) => {
+    return msg.send('https://mtgcardsmith.com/view/complete/full/2018/9/15/1537037410352905.png')
+  })
