5dfdebe4eccc4b26724669624269cb78c80e07e4
Cam Carter <contact@camcarter.com>
2019-01-09 10:39:41 -0500

    Prequel meme-ing with the best of 'em

---
+++ scripts/visible_confusion.js
@@ -0,0 +1,15 @@
+// Description:
+//   Visibly confused...
+// Dependencies:
+//   None
+// Configuration:
+//   None
+//
+// Commands:
+//   visible confusion - prequel memers unite
+//
+// Author:
+//   Anikan
+
+module.exports = robot =>
+  robot.hear(/\bvisible confusion\b/i, message => message.send('https://gph.is/2Qmy2Or'))
