c253e63cfc8bb12e6c6aceb934d87f9f4a8478c6
mark.easterling <mark.easterling@kroger.com>
2019-01-10 17:27:53 -0500

    more menus

--- scripts/menu.js
+++ scripts/menu.js
@@ -18,6 +18,13 @@ var getMenu = message =>
     const four = body[Math.floor(Math.random() * body.length)].word;
     const five = body[Math.floor(Math.random() * body.length)].word;
 
-    message.send(`Might I suggest ${one} served hot, on a bed of fresh ${two}? Followed by ${three} topped with ${four}, and ending with a decadent, glazed ${five}?`);
+    const menuArray = [
+      `Might I suggest ${one} served hot, on a bed of fresh ${two}? Followed by ${three} topped with ${four}, and ending with a decadent, glazed ${five}?`,
+      `Let’s start with a ${one} soup. Then, move on to mulberry wood smoked ${two} with a side of ${three}. For desert, how about ${four} pie with a scoop of ${five} ice cream?`,
+      `Braised ${one} with a creamy ${two} sauce, topped with ${three} infused ${four}.`,
+      `${one} and ${two} in aspic, and boiled ${three} with ${four} gravy.`
+    ]
+
+    message.send(menuArray[Math.floor(Math.random() * menuArray.length)]);
   })
 ;
