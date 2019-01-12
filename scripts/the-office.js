3714ccdb7df9f53d8c195b14c0b3dc64d5cf88a9
Tim M <tim.mecklem+gitkraken@gmail.com>
2019-01-12 12:52:15 -0500

    Learn the rules

--- scripts/the-office.js
+++ scripts/the-office.js
@@ -13,7 +13,14 @@
 // Author:
 //   Tim
 
-module.exports = robot =>
+module.exports = robot => {
   robot.hear(/\bpizza by alfredo\b/i, (msg) => {
     return msg.send('https://mtgcardsmith.com/view/complete/full/2018/9/15/1537037410352905.png')
   })
+
+  robot.hear(/\blearn the rules\b/i, (msg) => {
+    return msg.send('http://gph.is/1hbfQyK')
+  })
+}
+
+
